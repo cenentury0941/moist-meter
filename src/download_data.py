1e101fbd12cf04ccc958d5ac3f9c35e85f270ac6
meridati <meridatifoundation@gmail.com>
2024-03-03 11:48:58 +0530

    updated model

--- src/download_data.py
+++ src/download_data.py
@@ -3,7 +3,6 @@ import os
 
 zip_file_path = '../data/archive.zip'
 extract_dir = '../data/extracted/'
-os.makedirs(extract_dir, exist_ok=True)
 with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
     zip_ref.extractall(extract_dir)
 print('Zip file extracted successfully to:', extract_dir)
