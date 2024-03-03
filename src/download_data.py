3007d5d4fe376581ee88c22d4c1a7bd99a885450
meridati <meridatifoundation@gmail.com>
2024-03-03 12:04:34 +0530

    change path

--- src/download_data.py
+++ src/download_data.py
@@ -1,8 +1,8 @@
 import zipfile
 import os
 
-zip_file_path = '../data/archive.zip'
-extract_dir = '../data/extracted/'
+zip_file_path = os.getcwd() + '/app/data/archive.zip'
+extract_dir = os.getcwd() + '/app/data/extracted/'
 with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
     zip_ref.extractall(extract_dir)
 print('Zip file extracted successfully to:', extract_dir)
